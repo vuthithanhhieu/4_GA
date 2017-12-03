2f2dcb6500574b96447e153a13837eb098f0f59f
vuthithanhhieu <32197880+vuthithanhhieu@users.noreply.github.com>
2017-12-03 23:59:31 +0300

    Update Result.py

--- Result.py
+++ Result.py
@@ -1,9 +1,10 @@
 import json
 import requests
 
-result=json.dumps({"1": {"value": 6948, "weight": 10316, "volume": 12, "items": [1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1]}, "2": {"value": 4367, "weight": 10220, "volume": 12, "items": [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0]}})
+result=json.dumps({"1": {"value": 4388, "weight": 10316, "volume": 12, "items": [1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0]}, "2": {"value": 4435, "weight": 10220, "volume": 12, "items": [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0]}})
 print(result)
 url ='https://cit-home1.herokuapp.com/api/ga_homework'
 js = {'content-type': 'application/json'}
 r = requests.post(url, data=result,headers=js)
 print(r.json())
+
