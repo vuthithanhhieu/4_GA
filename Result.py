8aec56556750e5e1e51dfb69791b8b1e83049012
vuthithanhhieu <thanhhieubk50@gmail.com>
2017-12-03 20:07:17 +0300

    4_GA

---
+++ Result.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+result=json.dumps({"1": {"value": 6948, "weight": 10316, "volume": 12, "items": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}, "2": {"value": 4367, "weight": 10220, "volume": 13, "items": [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0]}})
+print(result)
+url ='https://cit-home1.herokuapp.com/api/ga_homework'
+js = {'content-type': 'application/json'}
+r = requests.post(url, data=result,headers=js)
+print(r.json())
